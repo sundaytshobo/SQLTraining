b93847f97f0c6ad6cd3f6b8e09df17dc28fe9d5c
sundaytshobo <sundaytshobo@gmail.com>
2025-04-09 20:36:20 -0400

    Add files via upload

---
+++ Food Co Query.sql
@@ -0,0 +1,20 @@
+Select *
+From [dbo].[FoodCo]
+
+--Foodco. table creation
+create table [FoodCo.]
+("S.no" int null,
+FruitsName varchar (50) not null,
+Amount int null)
+
+
+insert into [FoodCo.]
+([S.no], FruitsName, Amount)
+values
+(1,'Apple',150),
+(2,'Banana',75),
+(3,'Orange',60),
+(4,'Papaya',80),
+(5,'Mango',95),
+(6,'Apple',150),
+(7,'Lechi', null)
